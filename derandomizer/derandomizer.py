abc8c73f0970d7767d63fb080922f1e0b1efa70d
deimosian <deimosian@gmail.com>
2023-03-21 01:43:24 -0400

    add entity mods support

--- derandomizer/derandomizer.py
+++ derandomizer/derandomizer.py
@@ -4,22 +4,28 @@
 import os
 import shutil
 import subprocess
+from modules import entity_mods
 from modules import weapon_mods
 from modules import weapon_stats
 
 # Set directory and file name variables
 input_subdir = '/derandomizer/game_data'
 output_subdir = '/derandomizer/mod_data'
+entity_mods_file = '/scripts/blueprint_tables/entity_mods.dat'
 weapon_mods_file = '/scripts/blueprint_tables/weapon_mods.dat'
 weapon_stats_file = '/scripts/blueprint_tables/weapon_stats.dat'
-entity_mods_file = '/scripts/blueprint_tables/entity_mods.dat'
+
+# Modify the Entity Mods
+entity_mod_change_count = entity_mods.modify(input_subdir,output_subdir,entity_mods_file)
 
 # Modify the Weapon Mods
 weapon_mod_change_count = weapon_mods.modify(input_subdir,output_subdir,weapon_mods_file)
-print("Made " + str(weapon_mod_change_count) + " Weapon Mod Changes.")
 
 # Modify the Weapon Stats
 weapon_stats_change_count = weapon_stats.modify(input_subdir,output_subdir,weapon_stats_file)
+
+print("Made " + str(entity_mod_change_count) + " Entity Mod Changes.")
+print("Made " + str(weapon_mod_change_count) + " Weapon Mod Changes.")
 print("Made " + str(weapon_stats_change_count) + " Weapon Stats Changes.")
 
 shutil.make_archive('derandomizer', 'zip', "./" + output_subdir)
--- derandomizer/modules/entity_mods.py
+++ derandomizer/modules/entity_mods.py
@@ -1,1 +1,63 @@
 # Function to Remove Entity Mod Randomization from Riftbreaker
+
+def modify(input_subdir,output_subdir,entity_mods_file):
+
+	# Import Python RegEx module
+	import os
+	import re
+
+	# Move to the input directory and read the contents of the Weapon Mods file
+	script_dir = os.getcwd()
+	entity_mods_file_location = script_dir + input_subdir + entity_mods_file
+	# print(entity_mods_file_location)
+	with open(entity_mods_file_location, 'r') as f:
+		entity_mods_contents = f.read()
+
+	# Make a working copy of the contents
+	new_entity_mods_contents = entity_mods_contents
+
+	# Start the change counter
+	entity_mod_change_count = 0
+
+	# Bring all player weapons into a list of strings
+	entity_mods = re.findall(r'items/upgrades/.*?(?:\n})', entity_mods_contents, re.DOTALL)
+
+	# Work on each weapon string
+	for entity_mod in entity_mods:
+
+		#Make a working copy of the weapon
+		new_entity_mod = entity_mod
+
+		# Bring all weapon stats into a list of strings
+		moddescs = re.findall(r'EntityModDesc.*?(?:\n\t})', entity_mod, re.DOTALL)
+
+		for moddesc in moddescs:
+
+			#Make a working copy of the moddesc
+			new_moddesc = moddesc
+
+			random_flags = re.findall(r'.*random.*', new_moddesc)
+			for random_flag in random_flags:
+				# Remove the default value line
+				new_moddesc = re.sub(r'.*random.*\n','', new_moddesc)
+				entity_mod_change_count = entity_mod_change_count + 1
+
+			# Apply the change
+			new_entity_mod = new_entity_mod.replace(moddesc, new_moddesc)
+
+			# Delete the change
+			del new_moddesc
+		
+		# Replace the original weapon with the updated one
+		new_entity_mods_contents = new_entity_mods_contents.replace(entity_mod, new_entity_mod)
+
+		# Delete the update to prepare for another
+		del new_entity_mod
+
+	# Write the updated content to the output file
+	new_entity_mods_file_location = script_dir + output_subdir + entity_mods_file
+	with open(new_entity_mods_file_location, 'w') as f:
+		f.write(new_entity_mods_contents)
+
+	return entity_mod_change_count
+	
